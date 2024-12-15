91526fa6af03add6f35e111f7950d1c233ee2ed4
Alex Nichol <unixpickle@gmail.com>
2024-12-15 13:00:41 -0500

    remove norm

--- Sources/CellNet/Cell.swift
+++ Sources/CellNet/Cell.swift
@@ -29,7 +29,6 @@ public class Cell: Trainable {
     self.stateCount = stateCount
     super.init()
     self.layer1 = Linear(inCount: edgeCount + stateCount, outCount: hiddenSize)
-    self.norm = LayerNorm(shape: [hiddenSize])
     self.layer2 = Linear(inCount: hiddenSize, outCount: hiddenSize)
     self.layer3 = Linear(inCount: hiddenSize, outCount: stateCount * 2 + edgeCount)
     self.layer3.bias = Tensor(randnLike: self.layer3.bias!) * 0.01
@@ -37,10 +36,10 @@ public class Cell: Trainable {
 
   @recordCaller private func _callAsFunction(_ s: NetworkState) -> NetworkState {
     var h = Tensor(concat: [s.activations, s.cellStates], axis: -1)
+    h = h / h.pow(2).mean(axis: -1, keepdims: true).clamp(min: 1e-5).sqrt()
     let featureSize = edgeCount + stateCount
     h = h.reshape(h.shape[..<(h.shape.count - 1)] + [h.shape.last! / featureSize, featureSize])
     h = self.layer1(h)
-    h = self.norm(h)
     h = h.gelu()
     h = self.layer2(h)
     h = h.gelu()
