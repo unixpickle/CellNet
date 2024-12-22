67d05f141913e2e86818fdd129700d27682b1120
Alex Nichol <unixpickle@gmail.com>
2024-12-22 13:04:32 -0500

    refactor gate

--- Sources/TrainLogic/DataIterator.swift
+++ Sources/TrainLogic/DataIterator.swift
@@ -1,5 +1,21 @@
 import Honeycrisp
 
+enum LogicGate {
+  case or
+  case and
+  case xor
+
+  static func random() -> LogicGate { [Self.or, Self.and, Self.xor].randomElement()! }
+
+  func apply(_ x: Int, _ y: Int) -> Int {
+    switch self {
+    case .or: x | y
+    case .and: x & y
+    case .xor: x ^ y
+    }
+  }
+}
+
 struct DataIterator: Sequence, IteratorProtocol {
   let batchSize: Int
   let exampleCount: Int
@@ -8,20 +24,14 @@ struct DataIterator: Sequence, IteratorProtocol {
   mutating func next() -> ([Tensor], [Tensor])? {
     var allInputs = [Tensor]()
     var allLabels = [Tensor]()
-    let gates = (0..<batchSize).map { _ in Int.random(in: 0..<3) }
+    let gates = (0..<batchSize).map { _ in LogicGate.random() }
     for _ in 0..<exampleCount {
       var inputData = [Float]()
       var outputLabels = [Int]()
       for gate in gates {
         let in1 = Int.random(in: 0...1)
         let in2 = Int.random(in: 0...1)
-        let out =
-          switch gate {
-          case 0: in1 | in2
-          case 1: in1 & in2
-          case 2: in1 ^ in2
-          default: fatalError()
-          }
+        let out = gate.apply(in1, in2)
         inputData.append(Float(in1))
         inputData.append(1 - Float(in1))
         inputData.append(Float(in2))
