42e41b3e89221ba42787a7a27964e2fd9b131033
Alex Nichol <unixpickle@gmail.com>
2025-01-31 17:54:10 -0500

    shuffle on epoch boundaries

--- Sources/TrainMNIST/DataIterator.swift
+++ Sources/TrainMNIST/DataIterator.swift
@@ -30,6 +30,10 @@ struct DataIterator: Sequence, IteratorProtocol {
     var inputData = [Float]()
     var outputLabels = [Int]()
     for _ in 0..<batchSize {
+      if offset >= images.count {
+        permutation = Array(0..<images.count).shuffled()
+        offset = offset % images.count
+      }
       let img = images[permutation[offset % images.count]]
       for pixel in img.pixels { inputData.append(Float(pixel) / 255) }
       outputLabels.append(img.label)
